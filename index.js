4d9c57fae891ecf415ee3b4eb90f269795fdcf13
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-10 19:23:57 -0400

    commit tree c2c474c4ba3b9a9962c11c84046f7ba6e5f95007

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(){
+    let x = 2000
+    let b = 100
+    return x / b
+}
+
+const square = (parameter1) => parameter1 ** 2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
